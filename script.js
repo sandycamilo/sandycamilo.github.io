159f57ff4e81328acfc9924f597b791f185f89a0
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2023-11-10 22:06:35 -0500

    u

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach(circle => {
+    circle.addEventListener("click", function () {
+        const link = this.getAttribute("data-link");
+        if (link) {
+            window.location.href = link;
+        }
+    });
+
+    animateCircle(circle);
+});
+});
+
+function animateCircle(circle) {
+const maxX = window.innerWidth - circle.offsetWidth;
+const maxY = window.innerHeight - circle.offsetHeight;
+const animationDuration = 5000; 
+
+function updatePosition() {
+    const newX = Math.random() * maxX;
+    const newY = Math.random() * maxY;
+
+    circle.style.transform = `translate(${newX}px, ${newY}px)`;
+    
+    setTimeout(() => {
+        requestAnimationFrame(updatePosition);
+    }, animationDuration);
+}
+
+    requestAnimationFrame(updatePosition);
+}
